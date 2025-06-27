2005e3d7e79a0f3e363a3a6894e3c46458e65a2c
ali <ali_abde37@outlook.com>
2025-06-27 08:58:09 -0400

    Pydantic

--- Structured_output_typeddict.py
+++ Structured_output_typeddict.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict, Annotated, Optional
 
 
 load_dotenv()
@@ -8,11 +8,28 @@ load_dotenv()
 model = ChatOpenAI()
 
 class Review(TypedDict):
-    summary: str
-    sentiment: str
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[str, "Return sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
 
 structured_model = model.with_structured_output(Review)
 
-result = structured_model.invoke("""These are definitely not boyfriend jeans. They are somewhat loose skinny jeans at best. My thighs are big, but my calves aren’t, yet the thighs on these jeans are loose-ish while the calves are tight. They are midrise though and it’s so hard to find real midrise I’m willing to deal with them being skinny jeans. They are very stretchy. I ordered a 34 to be safe, but I should have ordered my usual 31-32(5’6, 220lbs). Although, if I had gone that small they probably would have ended up being true skinny jeans. The pockets are fine. I can fit my hand in but that’s about it. Honestly, if you are looking for good boyfriend jeans with pockets so deep you could hide a book in them, buy Judy Blues. These ain’t it.""")
+result = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it’s an absolute powerhouse! The Snapdragon 8 Gen 3 processor makes everything lightning fast—whether I’m gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver.
+
+The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera—the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x actually works well for distant objects, but anything beyond 30x loses quality.
+
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung’s One UI still comes with bloatware—why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+                                 
+Review by Nitish Singh""")
 
 print(result)
+print(result['summary'])
+print(result['sentiment'])
